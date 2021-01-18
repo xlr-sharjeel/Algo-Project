b2189d5849dc6fe22199d657c0eb0a2d62f573b7
xlr-sharjeel <hussainsharjeel75@gmsil.com>
2021-01-18 18:34:29 +0500

    Sharjeel

---
+++ lcs.js
@@ -0,0 +1,33 @@
+//LongestCommonSubsequence
+(function(){
+    const abc=document.getElementById('button');
+    abc.addEventListener('click', function () {
+            let i=5;
+            i=i+5;
+            document.write(i);
+        })
+})();
+
+// var str1 = require("prompt");
+// var str2 = require("prompt");
+
+// var str1=prompt("Enter First String")
+// var str2=prompt("Enter Second String")
+// var ans1=str1.split('')
+// var ans2=str2.split('')
+// let i,j
+// let res_arr=[]
+
+// for(i=str1.length; i>0; i--)
+// {
+//     for(j=str2.length; j>0; j--)
+//     {
+//         if(str2[j]==str1[i])
+//         {
+//             res_arr.push(str2[j])
+//             break
+//         }
+//     }
+// }
+
+// console.log(res_arr.reverse().join(''));
